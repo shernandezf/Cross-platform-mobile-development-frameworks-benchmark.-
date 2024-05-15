0a16c0a1dd8d9ed371f1bfeef47b705fa1a97a36
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-14 22:54:58 -0500

    added homescreen

--- Flutter/flutter_tesis_app/lib/main.dart
+++ Flutter/flutter_tesis_app/lib/main.dart
@@ -26,11 +26,21 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Main Screen")),
+      appBar: AppBar(title: Text("")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.network(
+              'https://github.com/shernandezf/resources/blob/main/logo_andes.jpg?raw=true',
+              width: 200, // Set your width as needed
+              height: 200, // Set your height as needed
+            ),
+            Text("Tesis Pregrado 2024 Santiago Hernandez",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+            Text("Profesor: Camilo Escobar Velasquez",
+                style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20), // Adds some space before the buttons
             ElevatedButton(
               onPressed: () => _navigateToWidget(
                   context,
