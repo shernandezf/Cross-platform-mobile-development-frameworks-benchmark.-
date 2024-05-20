7d15928af81ddb569a8531307f7321bd85c4c197
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-19 23:40:49 -0500

    flutter main finished

--- Flutter/flutter_tesis_app/lib/main.dart
+++ Flutter/flutter_tesis_app/lib/main.dart
@@ -33,31 +33,87 @@ class MainScreen extends StatelessWidget {
           children: <Widget>[
             Image.network(
               'https://github.com/shernandezf/resources/blob/main/logo_andes.jpg?raw=true',
-              width: 200, // Set your width as needed
-              height: 200, // Set your height as needed
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.height * 0.35,
+              fit: BoxFit.contain,
             ),
-            Text("Tesis Pregrado 2024 Santiago Hernandez",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
-            Text("Profesor: Camilo Escobar Velasquez",
-                style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            Text("Tesis Pregrado 2024 Santiago Hernández",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            Text("Profesor: Profesor: Camilo Escobar Velásquez",
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 20), // Adds some space before the buttons
-            ElevatedButton(
-              onPressed: () => _navigateToWidget(
-                  context,
-                  VideoPlayerWidget(
-                      videoUrl:
-                          'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')),
-              child: Text("Go to video player"),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: ElevatedButton(
+                onPressed: () => _navigateToWidget(
+                    context,
+                    VideoPlayerWidget(
+                        videoUrl:
+                            'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')),
+                child: Text("Video-Player"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xFFFFD700)), // Strong yellow color
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(
+                      MediaQuery.of(context).size.width * 0.92,
+                      MediaQuery.of(context).size.height * 0.06)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          7), // Change the degree of corner rounding
+                    ),
+                  ),
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () =>
-                  _navigateToWidget(context, ProximitySensorWidget()),
-              child: Text("Go to proximity sensor"),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: ElevatedButton(
+                onPressed: () =>
+                    _navigateToWidget(context, AccelerometerWidget()),
+                child: Text("Accelerometer"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xFFFFD700)), // Strong yellow color
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(
+                      MediaQuery.of(context).size.width * 0.92,
+                      MediaQuery.of(context).size.height * 0.06)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          7), // Change the degree of corner rounding
+                    ),
+                  ),
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () =>
-                  _navigateToWidget(context, AccelerometerWidget()),
-              child: Text("Go to accelerometer"),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: ElevatedButton(
+                onPressed: () =>
+                    _navigateToWidget(context, ProximitySensorWidget()),
+                child: Text("Proximity Sensor"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xFFFFD700)), // Strong yellow color
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(
+                      MediaQuery.of(context).size.width * 0.92,
+                      MediaQuery.of(context).size.height * 0.06)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          7), // Change the degree of corner rounding
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
